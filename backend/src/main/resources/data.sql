3786b560316d80ebd13a742685f3054db08f4172
Lucas Vinicius <howaitosame13@gmail.com>
2023-08-12 20:27:47 -0300

    enrollment seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -28,4 +28,6 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Neste capitulo vamos continuar...', 2, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQHoHWyiBD9XhNxmJxgnVssf0g3eqttUvsmNOljN5_QyhgDuh5KQtdqKTJ0a7WkRtJMoE&usqp=CAU', 1, 1);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Neste capitulo vamos finalizar...', 3, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQHoHWyiBD9XhNxmJxgnVssf0g3eqttUvsmNOljN5_QyhgDuh5KQtdqKTJ0a7WkRtJMoE&usqp=CAU', 1, 2);
 
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-05-13T20:50:07Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-05-13T20:50:07Z', null, true, false);
 
